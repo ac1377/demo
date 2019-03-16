26b00ac304f0e81f2ca11384a016f99dcdc7f05f
memor <2686603682@qq.com>
2019-03-17 00:34:51 +0800

    test push

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,10 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
         SpringApplication.run(DemoApplication.class, args);
     }
 
